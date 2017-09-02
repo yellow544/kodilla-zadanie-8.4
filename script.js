9bdb1acf38b7e89732dea0d9cbd0be560d207304
yellow544 <31159133+yellow544@users.noreply.github.com>
2017-09-02 22:57:19 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+/* 
+This is my first javascript comment.
+This is a block comment.
+*/
+//The prompt() method display a dialog box that prompts the visitor for input.
+var name = prompt('Enter your name');
+//The alert() method display as alert box with specified meesage and an OK button.
+alert('Hello, ' + name);
+//For debugging purposes, you can use console.log() method to display data.
+console.log('Hello, ' + name);
